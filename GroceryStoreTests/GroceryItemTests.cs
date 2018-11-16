e3c03ea7c09fdd11537767577b7ea04af92e6b4d
omccully <oscarmccully@gmail.com>
2018-11-16 12:39:51 -0500

    create EachesGroceryItem with constructor that sets Name and OriginalPrice

--- GroceryStoreTests/GroceryItemTests.cs
+++ GroceryStoreTests/GroceryItemTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryStore.Stock;
 
 namespace GroceryStoreTests
 {
@@ -7,10 +8,12 @@ namespace GroceryStoreTests
     public class GroceryItemTests
     {
         [TestMethod]
-        public void EachesMarkdownPrice_ReducesPurchasePrice()
+        public void EachesConstructor_SetsNameAndOriginalPrice()
         {
+            EachesGroceryItem item = new EachesGroceryItem("soup", 1.89M);
 
-
+            Assert.AreEqual("soup", item.Name);
+            Assert.AreEqual(1.89M, item.OriginalPrice);
         }
     }
 }
