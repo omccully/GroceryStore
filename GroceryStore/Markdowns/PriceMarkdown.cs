f57cb3df5dc4c8b9248e0696d7a1f2cb7ec913f4
omccully <oscarmccully@gmail.com>
2018-11-16 18:04:46 -0500

    create IPriceMarkdown and PriceMarkdown

---
+++ GroceryStore/Markdowns/PriceMarkdown.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Markdowns
+{
+    public class PriceMarkdown : IPriceMarkdown
+    {
+        decimal ReductionAmount;
+
+        public PriceMarkdown(decimal reductionAmount)
+        {
+            this.ReductionAmount = reductionAmount;
+        }
+
+        public decimal CalculateNewPrice(decimal originalPrice)
+        {
+            return originalPrice - ReductionAmount;
+        }
+    }
+}
---
+++ GroceryStoreTests/Markdowns/PriceMarkdownTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryStore.Stock;
+using GroceryStore.Cart;
+using GroceryStore.Markdowns;
+
+namespace GroceryStoreTests.Markdowns
+{
+    [TestClass]
+    public class PriceMarkdownTests
+    {
+        [TestMethod]
+        public void StaticPriceMarkdown_ReducesPurchasePrice()
+        {
+            IPriceMarkdown markdown = new PriceMarkdown(0.20M);
+
+            Assert.AreEqual(1.69M, markdown.CalculateNewPrice(1.89M));
+        }
+    }
+}
