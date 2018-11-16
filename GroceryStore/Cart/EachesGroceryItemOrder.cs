60bfbdebacd8e7e6cd02a57a4d04c8c8b5258ede
omccully <oscarmccully@gmail.com>
2018-11-16 12:56:39 -0500

    add EachesGroceryItemOrder class, implement constructor to set EachesGroceryItem and Count

---
+++ GroceryStore/Cart/EachesGroceryItemOrder.cs
@@ -0,0 +1,21 @@
+﻿using GroceryStore.Stock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Cart
+{
+    public class EachesGroceryItemOrder
+    {
+        public EachesGroceryItem Item { get; private set; }
+        public int Count { get; set; }
+
+        public EachesGroceryItemOrder(EachesGroceryItem item, int count = 1)
+        {
+            this.Item = item;
+            this.Count = count;
+        }
+    }
+}
---
+++ GroceryStoreTests/Cart/EachesGroceryItemOrderTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryStore.Stock;
+using GroceryStore.Cart;
+
+namespace GroceryStoreTests.Cart
+{
+    [TestClass]
+    public class EachesGroceryItemOrderTests
+    {
+        [TestMethod]
+        public void Constructor_SetsItemAndCount()
+        {
+            EachesGroceryItem item = new EachesGroceryItem("soup", 1.89M);
+            EachesGroceryItemOrder order = new EachesGroceryItemOrder(item, 3);
+
+            Assert.AreEqual(item, order.Item);
+            Assert.AreEqual(3, order.Count);
+        }
+    }
+}
