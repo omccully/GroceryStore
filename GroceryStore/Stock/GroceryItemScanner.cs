0dd1475e2b8fd7abca184253183da5de68024807
omccully <oscarmccully@gmail.com>
2018-11-17 13:29:12 -0500

    create GroceryItemScanner

---
+++ GroceryStore/Stock/GroceryItemScanner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Stock
+{
+    public class GroceryItemScanner
+    {
+        public List<EachesGroceryItem> Items;
+
+        public GroceryItemScanner()
+        {
+            Items = new List<EachesGroceryItem>();
+        }
+    }
+}
---
+++ GroceryStoreTests/Stock/GroceryItemScannerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GroceryStore.Stock;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroceryStoreTests.Stock
+{
+    [TestClass]
+    public class GroceryItemScannerTests
+    {
+        [TestMethod]
+        public void Items_CanAddEachesGroceryItem()
+        {
+            GroceryItemScanner scanner = new GroceryItemScanner();
+
+            Assert.AreEqual(0, scanner.Items.Count);
+            scanner.Items.Add(new EachesGroceryItem("soup", 1.89M));
+            Assert.AreEqual(1, scanner.Items.Count);
+        }
+    }
+}
