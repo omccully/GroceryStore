e36b08a0c4f98c8f6f6608f8e86470df294c8cb1
omccully <oscarmccully@gmail.com>
2018-11-17 11:16:58 -0500

    create BuyNGetUpToMDiscountedWeighedGroceryItemSpecial and cover case where not enough was bought for the special to apply

---
+++ GroceryStore/Specials/Weighed/BuyNGetUpToMDiscountedWeighedGroceryItemSpecial.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Specials.Weighed
+{
+    public class BuyNGetUpToMDiscountedWeighedGroceryItemSpecial
+    {
+        decimal RequiredWeight;
+        decimal DiscountPercentage;
+
+        public BuyNGetUpToMDiscountedWeighedGroceryItemSpecial(
+            decimal requiredWeight, decimal discountPercentage)
+        {
+            this.RequiredWeight = requiredWeight;
+            this.DiscountPercentage = discountPercentage;
+        }
+
+        public decimal CalculateNewPrice(decimal pricePerUnit, decimal weight)
+        {
+            return pricePerUnit * weight;
+        }
+
+        decimal DiscountMultiplier
+        {
+            get
+            {
+                return 1M - (DiscountPercentage / 100M);
+            }
+        }
+    }
+}
---
+++ GroceryStoreTests/Specials/Weighed/BuyNGetUpToMDiscountedWeighedGroceryItemSpecialTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryStore.Specials.Weighed;
+
+namespace GroceryStoreTests.Specials.Weighed
+{
+    [TestClass]
+    public class BuyNGetUpToMDiscountedWeighedGroceryItemSpecialTests
+    {
+        [TestMethod]
+        public void CalculateNewPrice_UsesRegularPrice_WhenNotBuyingEnough()
+        {
+            // buy up to 3.0 units, get up to 3.0 units free (100% off)
+            BuyNGetUpToMDiscountedWeighedGroceryItemSpecial special =
+                new BuyNGetUpToMDiscountedWeighedGroceryItemSpecial(3.0M, 100M);
+
+            Assert.AreEqual(2.00M, special.CalculateNewPrice(1.00M, 2.0M));
+        }
+    }
+}
