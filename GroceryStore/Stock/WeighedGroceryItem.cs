4797ee0843bc40b27d720bf62c018f2651522f22
omccully <oscarmccully@gmail.com>
2018-11-17 12:25:16 -0500

    create WeighedGroceryItem and test the constructor

---
+++ GroceryStore/Stock/WeighedGroceryItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Stock
+{
+    public class WeighedGroceryItem
+    {
+        public string Name { get; private set; }
+
+        public decimal OriginalPrice { get; set; }
+
+        public WeighedGroceryItem(string name, decimal originalPrice)
+        {
+            this.Name = name;
+            this.OriginalPrice = originalPrice;
+        }
+    }
+}
---
+++ GroceryStoreTests/Stock/WeighedGroceryItemTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryStore.Stock;
+
+namespace GroceryStoreTests.Stock
+{
+    [TestClass]
+    public class WeighedGroceryItemTests
+    {
+        [TestMethod]
+        public void Constructor_SetsNameAndOriginalPrice()
+        {
+            WeighedGroceryItem bananas = new WeighedGroceryItem("bananas", 2.38M);
+
+            Assert.AreEqual("bananas", bananas.Name);
+            Assert.AreEqual(2.38M, bananas.OriginalPrice);
+        }
+    }
+}
