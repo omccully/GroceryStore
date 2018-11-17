b9e065ff89bd148236fa18ef4158585ad3341b88
omccully <oscarmccully@gmail.com>
2018-11-17 12:59:31 -0500

    create WeighedGroceryItemOrder

---
+++ GroceryStore/Cart/WeighedGroceryItemOrder.cs
@@ -0,0 +1,22 @@
+﻿using GroceryStore.Stock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Cart
+{
+    public class WeighedGroceryItemOrder
+    {
+        public WeighedGroceryItem Item { get; private set; }
+        public decimal Weight { get; set; }
+
+        public WeighedGroceryItemOrder(WeighedGroceryItem item, decimal weight)
+        {
+            this.Item = item;
+            this.Weight = weight;
+        }
+
+    }
+}
---
+++ GroceryStoreTests/Cart/WeighedGroceryItemOrderTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GroceryStore.Stock;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryStore.Cart;
+
+namespace GroceryStoreTests.Cart
+{
+    [TestClass]
+    public class WeighedGroceryItemOrderTests
+    {
+        [TestMethod]
+        public void Constructor_SetsItemAndWeight()
+        {
+            decimal weight = 1.6M;
+            WeighedGroceryItem bananas = new WeighedGroceryItem("bananas", 2.30M);
+            WeighedGroceryItemOrder order = new WeighedGroceryItemOrder(bananas, weight);
+
+            Assert.AreEqual(bananas, order.Item);
+            Assert.AreEqual(weight, order.Weight);
+        }
+    }
+}
