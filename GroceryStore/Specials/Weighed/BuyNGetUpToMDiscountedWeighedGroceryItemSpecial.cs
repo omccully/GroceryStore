ab5d7392bf11165765f964b0d0baf4aa36cac09d
omccully <oscarmccully@gmail.com>
2018-11-17 11:34:14 -0500

    cover case for BuyNGetMDiscountedEachesGroceryItemSpecial when buying a multiple of the full deal weight

--- GroceryStore/Specials/Weighed/BuyNGetUpToMDiscountedWeighedGroceryItemSpecial.cs
+++ GroceryStore/Specials/Weighed/BuyNGetUpToMDiscountedWeighedGroceryItemSpecial.cs
@@ -18,9 +18,33 @@ namespace GroceryStore.Specials.Weighed
             this.DiscountPercentage = discountPercentage;
         }
 
-        public decimal CalculateNewPrice(decimal pricePerUnit, decimal weight)
+        public decimal CalculateNewPrice(decimal pricePerUnit, decimal itemWeight)
         {
-            return pricePerUnit * weight;
+            decimal discountPrice = pricePerUnit * DiscountMultiplier;
+
+            if (itemWeight % WeightPerFullDeal == 0)
+            {
+                decimal fullDealCount = Math.Round(itemWeight / WeightPerFullDeal);
+
+                decimal fullDealNondiscountedWeight = RequiredWeight * fullDealCount;
+                decimal fullDealNondiscountedPrice = fullDealNondiscountedWeight * pricePerUnit;
+
+                decimal fullDealDiscountedWeight = fullDealNondiscountedWeight;
+                decimal fullDealDiscountedPrice = fullDealDiscountedWeight * discountPrice;
+
+
+                return fullDealNondiscountedPrice + fullDealDiscountedPrice;
+            }
+            
+            return pricePerUnit * itemWeight;
+        }
+
+        decimal WeightPerFullDeal
+        {
+            get
+            {
+                return RequiredWeight * 2;
+            }
         }
 
         decimal DiscountMultiplier
--- GroceryStoreTests/Specials/Weighed/BuyNGetUpToMDiscountedWeighedGroceryItemSpecialTests.cs
+++ GroceryStoreTests/Specials/Weighed/BuyNGetUpToMDiscountedWeighedGroceryItemSpecialTests.cs
@@ -18,5 +18,19 @@ namespace GroceryStoreTests.Specials.Weighed
 
             Assert.AreEqual(2.00M, special.CalculateNewPrice(1.00M, 2.0M));
         }
+
+        [TestMethod]
+        public void CalculateNewPrice_HalfTheWeightIsDiscounted_WhenBuyingTwiceTheRequiredAmount()
+        {
+            // buy up to 3.0 units, get up to 3.0 units 25% off
+            BuyNGetUpToMDiscountedWeighedGroceryItemSpecial special =
+                new BuyNGetUpToMDiscountedWeighedGroceryItemSpecial(3.0M, 25M);
+
+            // 3.0 * 1.00 = 3.00
+            // 3.0 * 0.75 = 2.25
+            // = 5.25
+
+            Assert.AreEqual(5.25M, special.CalculateNewPrice(1.00M, 6.0M));
+        }
     }
 }
