7411e31ed581535df65ebc4a824a5b328ac7cdbd
omccully <oscarmccully@gmail.com>
2018-11-17 13:12:54 -0500

    create GroceryCart

---
+++ GroceryStore/Cart/GroceryCart.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Cart
+{
+    public class GroceryCart
+    {
+        public List<EachesGroceryItemOrder> Orders { get; set; }
+
+        public GroceryCart()
+        {
+            Orders = new List<EachesGroceryItemOrder>();
+        }
+    }
+}
---
+++ GroceryStoreTests/Cart/GroceryCartTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GroceryStore.Cart;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GroceryStoreTests.Cart
+{
+    [TestClass]
+    public class GroceryCartTests
+    {
+        [TestMethod]
+        public void Orders_CanAddEachesItem()
+        {
+            EachesGroceryItemOrder eachesOrder = new EachesGroceryItemOrder(null, 3);
+
+            GroceryCart cart = new GroceryCart();
+
+            Assert.AreEqual(0, cart.Orders.Count);
+            cart.Orders.Add(eachesOrder);
+            Assert.AreEqual(1, cart.Orders.Count);
+        }
+    }
+}
