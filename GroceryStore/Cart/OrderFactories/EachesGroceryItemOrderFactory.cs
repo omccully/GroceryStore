a71a75ab89b9e01f172e081a02b9e36dcfbcb590
omccully <oscarmccully@gmail.com>
2018-11-20 13:14:13 -0500

    create EachesGroceryItemOrderFactory

---
+++ GroceryStore/Cart/OrderFactories/EachesGroceryItemOrderFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroceryStore.Stock;
+
+namespace GroceryStore.Cart.OrderFactories
+{
+    public class EachesGroceryItemOrderFactory : IGroceryItemOrderFactory
+    {
+        const int DefaultEachesItemCount = 1;
+
+        public IGroceryItemOrder CreateOrder(IGroceryItem item)
+        {
+            IEachesGroceryItem eachesItem = item as EachesGroceryItem;
+
+            return new EachesGroceryItemOrder(eachesItem, DefaultEachesItemCount);
+        }
+    }
+}
---
+++ GroceryStoreTests/Cart/OrderFactories/EachesGroceryItemOrderFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GroceryStore.Cart;
+using GroceryStore.Cart.OrderFactories;
+using GroceryStore.Stock;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GroceryStoreTests.Cart.OrderFactories
+{
+    [TestClass]
+    public class EachesGroceryItemOrderFactoryTests
+    {
+        [TestMethod]
+        public void CreateOrder_CreatesOrderWithOneItem_IfIsIEachesGroceryItem()
+        {
+            EachesGroceryItem item = new EachesGroceryItem("soup", 1.89M);
+
+            EachesGroceryItemOrderFactory factory =
+                new EachesGroceryItemOrderFactory();
+
+            IGroceryItemOrder order = factory.CreateOrder(item);
+            Assert.IsTrue(order is EachesGroceryItemOrder);
+            EachesGroceryItemOrder eachesOrder = ((EachesGroceryItemOrder)order);
+            Assert.AreEqual(item, eachesOrder.Item);
+            Assert.AreEqual(1, eachesOrder.Count);
+        }
+    }
+}
