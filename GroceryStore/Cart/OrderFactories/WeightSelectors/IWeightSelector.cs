9fdfd9a7efe23aaad4c27f53bd96c595d564aa62
omccully <oscarmccully@gmail.com>
2018-11-20 13:56:56 -0500

    create StaticWeightSelector

--- GroceryStore/Cart/OrderFactories/WeightSelectors/IWeightSelector.cs
+++ GroceryStore/Cart/OrderFactories/WeightSelectors/IWeightSelector.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GroceryStore.Cart.OrderFactories
+namespace GroceryStore.Cart.OrderFactories.WeightSelectors
 {
     public interface IWeightSelector
     {
---
+++ GroceryStore/Cart/OrderFactories/WeightSelectors/StaticWeightSelector.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryStore.Cart.OrderFactories.WeightSelectors
+{
+    class StaticWeightSelector : IWeightSelector
+    {
+        decimal StaticWeight;
+
+        public StaticWeightSelector(decimal staticWeight)
+        {
+            this.StaticWeight = staticWeight;
+        }
+
+        public decimal SelectWeight()
+        {
+            return StaticWeight;
+        }
+    }
+}
