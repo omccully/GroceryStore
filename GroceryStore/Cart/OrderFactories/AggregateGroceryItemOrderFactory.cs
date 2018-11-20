7b75b49ed6df272f784951661362d53e946bccda
omccully <oscarmccully@gmail.com>
2018-11-20 14:21:46 -0500

    create AggregateGroceryItemOrderFactory

---
+++ GroceryStore/Cart/OrderFactories/AggregateGroceryItemOrderFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroceryStore.Stock;
+
+namespace GroceryStore.Cart.OrderFactories
+{
+    public class AggregateGroceryItemOrderFactory : IGroceryItemOrderFactory
+    {
+        List<IGroceryItemOrderFactory> Factories;
+
+        public AggregateGroceryItemOrderFactory(
+            params IGroceryItemOrderFactory[] factories)
+            : this((IEnumerable<IGroceryItemOrderFactory>)factories)
+        {
+            
+        }
+
+        public AggregateGroceryItemOrderFactory(
+            IEnumerable<IGroceryItemOrderFactory> factories)
+        {
+            Factories = new List<IGroceryItemOrderFactory>(factories);
+        }
+
+        public IGroceryItemOrder CreateOrder(IGroceryItem item)
+        {
+            foreach(IGroceryItemOrderFactory factory in Factories)
+            {
+                try
+                {
+                    return factory.CreateOrder(item);
+                }
+                catch(InvalidGroceryItemTypeException)
+                {
+
+                }
+            }
+
+            throw new InvalidGroceryItemTypeException();
+        }
+    }
+}
---
+++ GroceryStoreTests/Cart/OrderFactories/AggregateGroceryItemOrderFactoryTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using GroceryStore.Cart;
+using GroceryStore.Cart.OrderFactories;
+using GroceryStore.Stock;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GroceryStoreTests.Cart.OrderFactories
+{
+    [TestClass]
+    public class AggregateGroceryItemOrderFactoryTests
+    {
+        [TestMethod]
+        public void CreateOrder_UsesSecondFactory_WhenFirstFactoryThrowsException()
+        {
+            IGroceryItemOrder expectedOrder = new Mock<IGroceryItemOrder>().Object;
+
+            Mock<IGroceryItemOrderFactory> firstFactory = new Mock<IGroceryItemOrderFactory>();
+            firstFactory.Setup(f => f.CreateOrder(It.IsAny<IGroceryItem>()))
+                .Throws<InvalidGroceryItemTypeException>();
+
+            Mock<IGroceryItemOrderFactory> secondFactory = new Mock<IGroceryItemOrderFactory>();
+            secondFactory.Setup(f => f.CreateOrder(It.IsAny<IGroceryItem>()))
+                .Returns(expectedOrder);
+
+            AggregateGroceryItemOrderFactory factory =
+                new AggregateGroceryItemOrderFactory(firstFactory.Object, 
+                secondFactory.Object);
+
+            Assert.AreEqual(expectedOrder, factory.CreateOrder(null));
+        }
+
+        [TestMethod]
+        public void CreateOrder_UsesFirstFactory_WhenFirstFactoryReturnsResult()
+        {
+            IGroceryItemOrder expectedOrder = new Mock<IGroceryItemOrder>().Object;
+            IGroceryItemOrder unexpectedOrder = new Mock<IGroceryItemOrder>().Object;
+
+            Mock<IGroceryItemOrderFactory> firstFactory = new Mock<IGroceryItemOrderFactory>();
+            firstFactory.Setup(f => f.CreateOrder(It.IsAny<IGroceryItem>()))
+                .Returns(expectedOrder);
+
+            Mock<IGroceryItemOrderFactory> secondFactory = new Mock<IGroceryItemOrderFactory>();
+            secondFactory.Setup(f => f.CreateOrder(It.IsAny<IGroceryItem>()))
+                .Returns(unexpectedOrder);
+
+            AggregateGroceryItemOrderFactory factory =
+                new AggregateGroceryItemOrderFactory(firstFactory.Object,
+                secondFactory.Object);
+
+            Assert.AreEqual(expectedOrder, factory.CreateOrder(null));
+            Assert.AreNotEqual(unexpectedOrder, factory.CreateOrder(null));
+        }
+
+    }
+}
