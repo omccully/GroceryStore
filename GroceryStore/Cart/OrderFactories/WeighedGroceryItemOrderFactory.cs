a34a18aad92d1fce541fc03e6d757e08a6adaa4d
omccully <oscarmccully@gmail.com>
2018-11-20 13:30:39 -0500

    create WeighedGroceryItemOrderFactory

---
+++ GroceryStore/Cart/OrderFactories/WeighedGroceryItemOrderFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GroceryStore.Stock;
+
+namespace GroceryStore.Cart.OrderFactories
+{
+    public class WeighedGroceryItemOrderFactory : IGroceryItemOrderFactory
+    {
+        decimal DefaultWeighedItemWeight;
+
+        public WeighedGroceryItemOrderFactory()
+        {
+            this.DefaultWeighedItemWeight = 0.0M;
+        }
+
+        public IGroceryItemOrder CreateOrder(IGroceryItem item)
+        {
+            IWeighedGroceryItem weighedItem = item as IWeighedGroceryItem;
+
+            if (weighedItem == null)
+                throw new InvalidGroceryItemTypeException();
+
+            return new WeighedGroceryItemOrder(weighedItem, DefaultWeighedItemWeight);
+        }
+    }
+}
---
+++ GroceryStoreTests/Cart/OrderFactories/WeighedGroceryItemOrderFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GroceryStore.Cart;
+using GroceryStore.Stock;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GroceryStore.Cart.OrderFactories;
+using Moq;
+
+namespace GroceryStoreTests.Cart.OrderFactories
+{
+    [TestClass]
+    public class WeighedGroceryItemOrderFactoryTests
+    {
+        [TestMethod]
+        public void CreateOrder_CreatesOrderWithNoWeight_IfIsIWeighedGroceryItem()
+        {
+            WeighedGroceryItem item = new WeighedGroceryItem("bananas", 2.38M);
+
+            WeighedGroceryItemOrderFactory factory =
+                new WeighedGroceryItemOrderFactory();
+
+            IGroceryItemOrder order = factory.CreateOrder(item);
+            Assert.IsTrue(order is WeighedGroceryItemOrder);
+            WeighedGroceryItemOrder weighedOrder = ((WeighedGroceryItemOrder)order);
+            Assert.AreEqual(item, weighedOrder.Item);
+            Assert.AreEqual(0.0M, weighedOrder.Weight);
+        }
+    }
+}
